69ba4a532970b438677783192eea9561c234062f
Pedro Eduardo <edu_452009@hotmail.com>
2022-06-04 09:24:39 -0500

    segundo proyecto

---
+++ 04_Ejercicios/Quiz-app/js/script.js
@@ -0,0 +1,40 @@
+const quizData=[
+    {
+        question: 'how old is florin',
+        a: '10',
+        b: '20',
+        c: '30',
+        d: '40',
+        correct: 'c'
+    },
+    {question:'what is the most used programing lenguage in 2019',
+    a:'java',
+    b:'c++',
+    c:'python',
+    d:'javascript',
+    correct: 'a'
+},
+    {question:'who is the president of us',  
+    a:'florin poppins',
+    b:'Donald Trump',
+    c:'ivan sedano',
+    d:'mihai andrei',
+    correct: 'b'
+}   ,
+    {question:'what does html stand for',  
+    a:'hypertext markup lenguage',
+    b:'cascading style sheet',
+    c:'jason object notation',
+    d:'heliopter terminals motorboats lamborginis',
+    correct: 'a'
+    }
+    ,
+    {question:'what year was javascript launched',  
+    a:'1996',
+    b:'1995',
+    c:'1994',
+    d:'none of the above',
+    correct: 'a'
+    }
+
+]
